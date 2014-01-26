696123b92705c5d6810597783dc8a9d42b430bbd
Ed Wheeler <ewheeler3@gmail.com>
2014-01-26 17:55:35 -0500

    moved nav to action bar, started checkbox in prefs to keep screen on

--- JWSongs/src/main/java/com/pelletized/jwsongs/MainActivity.java
+++ JWSongs/src/main/java/com/pelletized/jwsongs/MainActivity.java
@@ -2,15 +2,20 @@ package com.pelletized.jwsongs;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+    WebView webView;
+    String appUrl = "file:///android_asset/index.html";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +23,18 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         //the webview
-        WebView webView = (WebView) findViewById(R.id.webView);
+        webView = (WebView) findViewById(R.id.webView);
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webSettings.setDefaultFontSize(getFontSize());
-        webView.loadUrl("file:///android_asset/index.html");
+        //appUrl = "file:///android_asset/index.html";
+        webView.loadUrl(appUrl);
+
+        //getScreenPrefs();
 
         //the home button
-        //getActionBar().setHomeButtonEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     public int getFontSize() {
@@ -50,6 +59,25 @@ public class MainActivity extends Activity {
         return fontSizeValue;
     }
 
+
+
+
+    private boolean getScreenPrefs() {
+        SharedPreferences getPrefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+        boolean screenPrefs = getPrefs.getBoolean("sleepPrefs", true);
+
+        if(screenPrefs) {
+            Log.d("mainactivity","screen should stay ON");
+            getWindow().addFlags(android.view.WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        } else {
+            Log.d("mainactivity","screen should be OFF");
+            getWindow().clearFlags(android.view.WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        }
+
+        return true;
+    }
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -63,9 +91,23 @@ public class MainActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         switch (item.getItemId()) {
+            //settings menu item
             case R.id.action_settings:
                 Intent intent = new Intent("com.pelletized.jwsongs.PREFS");
                 startActivityForResult(intent, R.id.action_settings);
+                return true;
+
+            case R.id.rateApp:
+                final String appPackageName = getPackageName();
+                try {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appPackageName)));
+                } catch (android.content.ActivityNotFoundException anfe) {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://play.google.com/store/apps/details?id=" + appPackageName)));
+                }
+            break;
+            //home button
+            case android.R.id.home:
+                webView.loadUrl(appUrl);
                 return true;
         }
         return super.onOptionsItemSelected(item);
@@ -75,7 +117,7 @@ public class MainActivity extends Activity {
     protected void onActivityResult(int req, int result, Intent data) {
         switch( req ) {
             case R.id.action_settings:
-                WebView webView = (WebView) findViewById(R.id.webView);
+                webView = (WebView) findViewById(R.id.webView);
                 WebSettings webSettings = webView.getSettings();
                 webSettings.setDefaultFontSize(getFontSize());
                 webView.reload();
@@ -83,5 +125,20 @@ public class MainActivity extends Activity {
         }
     }
 
+    private static long back_pressed;
+
+    @Override
+    public void onBackPressed()
+    {
+        if (back_pressed + 1000 > System.currentTimeMillis()) {
+            super.onBackPressed();
+        }
+
+        else {
+            webView.reload();
+            Toast.makeText(getBaseContext(), "Press back again to exit", Toast.LENGTH_SHORT).show();
+        }
+        back_pressed = System.currentTimeMillis();
+    }
 
 }
--- JWSongs/src/main/java/com/pelletized/jwsongs/prefsActivity.java
+++ JWSongs/src/main/java/com/pelletized/jwsongs/prefsActivity.java
@@ -1,8 +1,14 @@
 package com.pelletized.jwsongs;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+import android.util.Log;
 
 public class prefsActivity extends PreferenceActivity {
     @Override
@@ -10,7 +16,28 @@ public class prefsActivity extends PreferenceActivity {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.prefs);
 
+        PreferenceManager.setDefaultValues(this, R.xml.prefs, false);
 
-        //webView.reload();
+        final CheckBoxPreference sleepPrefs = (CheckBoxPreference) getPreferenceManager().findPreference("sleepPrefs");
+        sleepPrefs.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                Log.d("prefsactivity","thing got clicked");
+
+                if (sleepPrefs.isChecked()){
+                    Log.d("prefsactivity","thing IS CHECKED");
+                    getWindow().addFlags(android.view.WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+                } else {
+                    Log.d("prefsactivity","thing is NOT");
+                    getWindow().clearFlags(android.view.WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+                }
+                return true;
+            }
+
+        });
+
+
+
+
     }
 }
