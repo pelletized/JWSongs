6b18d08d7e3e5f845b9cf7c5197e7feba2f0b596
Ed Wheeler <ewheeler3@gmail.com>
2014-03-08 11:45:18 -0500

    home button added to settings activity

--- JWSongs/src/main/java/com/pelletized/jwsongs/prefsActivity.java
+++ JWSongs/src/main/java/com/pelletized/jwsongs/prefsActivity.java
@@ -3,6 +3,7 @@ package com.pelletized.jwsongs;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
 import android.preference.PreferenceActivity;
+import android.view.MenuItem;
 
 public class prefsActivity extends PreferenceActivity {
 
@@ -13,7 +14,21 @@ public class prefsActivity extends PreferenceActivity {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.prefs);
 
+        getActionBar().setHomeButtonEnabled(true);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            finish(); //end activity and go back to main
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
